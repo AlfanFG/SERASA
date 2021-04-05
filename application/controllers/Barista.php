8d5e97176e4b07f3a2192523e1d61dd392a440c9
Rafli Firdaus <firdaus.rafli24@gmail.com>
2021-04-05 15:07:14 +0700

    Commit Controller

---
+++ application/controllers/Barista.php
@@ -0,0 +1,26 @@
+<?php
+defined('BASEPATH') or exit('No direct script access allowed');
+
+class Barista extends CI_Controller
+{
+
+    function __construct()
+    {
+        parent::__construct();
+
+        if ($this->session->userdata('status') != "login") {
+            redirect(base_url("login"));
+        }
+    }
+
+    public function index()
+    {
+        // $nama = $_SESSION['Nama'];
+        // $jabatan = $_SESSION['idJabatan'];
+        // echo 'Selamat datang' . ' ' . $nama . '. ' . 'Di halaman Manajer';
+
+        // $data['tbl_pegawai'] = $this->db->get_where('tbl_pegawai', ['id_pegawai' => $this->session->userdata('id_pegawai')])->row_array();
+        // echo 'Selamat datang' . $data['tbl_pegawai']['namaPegawai'];
+        $this->load->view('barista/index');
+    }
+}
--- application/models/KategoriMenu_m.php
+++ application/models/KategoriMenu_m.php
@@ -7,6 +7,19 @@ class KategoriMenu_m extends CI_Model
         return $this->db->get('tbl_kategorimenu')->result_array();
     }
 
+    public function idKategoriMenu()
+    {
+        $query = $this->db->query(
+            "SELECT IFNULL(MAX(SUBSTRING(id_kategoriMenu,5)),0)+1 AS no_urut FROM tbl_kategoriMenu"
+        );
+        $data = $query->row_array();
+        $no_urut = sprintf("%'.04d", $data['no_urut']);
+
+        $id = 'K' . $no_urut;
+
+
+        return $id;
+    }
     public function addKategoriMenu()
     {
         $data = [
