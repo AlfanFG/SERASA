f03d0543327f557d360d5360e69812e6bd52bf3e
AlfanFG <alfanfaturahman10@gmail.com>
2021-05-10 14:48:57 +0700

    Nav Manajer

--- application/views/parts/navigationManajer.php
+++ application/views/parts/navigationManajer.php
@@ -155,7 +155,7 @@
                                     </a>
                                 </li>
                                 <li class="nav-item">
-                                    <a href="pages/charts/flot.html" class="nav-link">
+                                    <a href="<?php echo base_url('HistoryPesanan') ?>" class="nav-link">
                                         <i class="far fa-circle nav-icon"></i>
                                         <p>Data Pemesanan</p>
                                     </a>
